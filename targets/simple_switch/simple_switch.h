fdbff8f86e401a49a44dad899c51bcfcad84ff9f
stevenchiu30801 <steven30801@gmail.com>
2020-01-16 13:07:26 +0800

    enable priority queue in simple switch

--- targets/simple_switch/simple_switch.h
+++ targets/simple_switch/simple_switch.h
@@ -40,7 +40,7 @@
 // you can also choose the field from which the priority value will be read, as
 // well as the number of priority queues per port
 // PRIORITY 0 IS THE LOWEST PRIORITY
-// #define SSWITCH_PRIORITY_QUEUEING_ON
+#define SSWITCH_PRIORITY_QUEUEING_ON
 
 #ifdef SSWITCH_PRIORITY_QUEUEING_ON
 #define SSWITCH_PRIORITY_QUEUEING_NB_QUEUES 8
